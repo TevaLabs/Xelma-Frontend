aeee2c3f487da6ec68d17433c163e760eb2fa55a
KingFRANKHOOD <chibuezemfrancis@gmail.com>
2026-01-22 22:37:43 +0100

    feat: build learn section with lessons list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import './App.css'
 import Header from './components/Header'
-
+import { LearnSection } from "@/components/LearnSection";
 
 function App() {
-
-
   return (
     <>
-
       <Header />
 
+      {/* Outer Container with your exact specs */}
+      <div
+        className="fixed bottom-6 right-6 bg-white shadow-2xl z-50 flex flex-col items-center"
+        style={{
+          width: '430px',
+          height: '310px',
+          borderRadius: '24px',
+          padding: '10px',
+          opacity: 1,
+          border: '1px solid #e5e7eb'
+        }}
+      >
+        <LearnSection />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
